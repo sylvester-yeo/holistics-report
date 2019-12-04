d5f9affbd51ded67a76ca2914fb215709a430035
sylvester.yeo@grabtaxi.com <sylvester.yeo@grabtaxi.com>
2019-12-04 12:00:47 +0800

    2019-12-04

--- 116820 - Mex Payment Report.sql
+++ 116820 - Mex Payment Report.sql
@@ -1,10 +1,49 @@
 /*116820*/
+with fo as (
+    SELECT
+        json_extract(snapshot_detail,'$.cartWithQuote.discounts') AS array_discount
+        ,(case when json_extract_scalar(snapshot_detail, '$.newTaxFlow') = 'true' then
+                    cast(json_extract_scalar(snapshot_detail, '$.cartWithQuote.foodQuoteInMin.priceInMinorUnit') as double) - COALESCE(cast(json_extract_scalar(snapshot_detail, '$.cartWithQuote.foodQuoteInMin.inclTaxInMin') as double),0)
+                    else cast(json_extract_scalar(snapshot_detail, '$.cartWithQuote.foodQuoteInMin.priceInMinorUnit') as double) end)/100 as order_value_pre_tax
+        ,COALESCE(cast(json_extract_scalar(snapshot_detail, '$.cartWithQuote.foodQuoteInMin.inclTaxInMin') as double)/100,0) as tax_value
+        ,(case when json_extract_scalar(snapshot_detail, '$.newTaxFlow') = 'true' then cast(json_extract_scalar(snapshot_detail, '$.cartWithQuote.foodQuoteInMin.priceInMinorUnit') as double)
+                    else cast(json_extract_scalar(snapshot_detail, '$.cartWithQuote.foodQuoteInMin.priceInMinorUnit') as double) + COALESCE(cast(json_extract_scalar(snapshot_detail, '$.cartWithQuote.foodQuoteInMin.inclTaxInMin') as double),0) end)/100
+                    as order_value_with_tax
+        ,(cast(coalesce(json_extract_scalar(snapshot_detail, '$.cartWithQuote.totalQuoteInMin.mexCommission'),'0.0') as double) + cast(coalesce(json_extract_scalar(snapshot_detail, '$.cartWithQuote.totalQuoteInMin.gkCommission'),'0.0') as double))/100 as mex_commission
+        ,coalesce(cast(json_extract_scalar(json_parse(snapshot_detail), '$.cartWithQuote.promoCodes[0].promoAmountInMin') as double),0) / power(double '10.0', coalesce(cast(json_extract_scalar(snapshot_detail, '$.currency.exponent') as int),0))
+            as promo_expense
+        ,coalesce(json_extract_scalar(snapshot_detail, '$.cartWithQuote.promoCodes[0].promoCode'),'') as promo_code
+        ,coalesce(cast(json_extract(cast(json_extract(json_parse(snapshot_detail), '$.cartWithQuote.merchantCartWithQuoteList') as array<json>)[1], '$.merchantInfoObj.commission') as double), fo.commission) as commission_rate
+        ,coalesce(cast(json_extract(cast(json_extract(json_parse(snapshot_detail), '$.cartWithQuote.merchantCartWithQuoteList') as array<json>)[1], '$.merchantInfoObj.taxRate') as double), fo.tax) as tax_perc
+        ,*
+    FROM public.prejoin_food_order fo
+    WHERE [[country_id in ({{country|noquote}})]]
+        and [[city_id in ({{cities|noquote}})]]
+        and [[merchant_id in ({{merchant}})]]
+        and [[partition_date >= date_format(date({{ order_create_start_date }}) - interval '1' DAY, '%Y-%m-%d')]]
+        and [[partition_date <= date_format(date({{ order_create_end_date }}) + interval '1' DAY , '%Y-%m-%d')]]
+        {{#if delivery_option == 'takeaway'}}
+		AND json_extract_scalar(fo.snapshot_detail, '$.deliveryOption') = '1'
+		{{#endif}}
+		{{#if delivery_option == 'delivery'}}
+		AND (json_extract_scalar(fo.snapshot_detail, '$.deliveryOption') = '0'
+		OR json_extract_scalar(fo.snapshot_detail, '$.deliveryOption') is null)
+		{{#endif}}
+)
+, mex_funded_fo as (
+    select
+        order_id
+        ,sum(CAST(json_extract_scalar(discount,'$.mexFundedAmountInMin') AS double)/100) AS total_mex_funded_discount
+    from fo
+    CROSS JOIN UNNEST(CAST(array_discount AS array(json))) AS a(discount)
+    group by 1
+)
 SELECT
      cities.name as city_name
 	  ,cities.country_code as country_name
 	  ,fo.city_id
 	  ,fo.country_id
-      ,fo.order_id as order_id
+        ,fo.order_id as order_id
 	  ,fo.last_booking_code as booking_code
 	  ,fo.short_order_number as short_order_id
 
@@ -53,13 +92,13 @@ SELECT
 	  ,case when lower(mex.name) like '%domino%' then fo.promo_code else bb.promo_code end as promo_code
 	  --,bb.promo_code
 	  ,case when lower(mex.name) like '%domino%' then fo.promo_expense else bb.promo_expense end as promo_expense
-	  ,ROUND(coalesce(fo.order_value, bb.food_sub_total),2) as order_value
-	  ,ROUND(fo.mexFunded, 2) as mexFunded
+	  ,ROUND(coalesce(fo.order_value_pre_tax, bb.food_sub_total),2) as order_value
+	  ,ROUND(coalesce(mex_funded_fo.total_mex_funded_discount,0), 2) as mexFunded
 	  ,ROUND(coalesce(fo.tax_perc, fo.tax),2) as tax_perc
 	  ,ROUND(fo.tax_value,2) as tax_value
 	  ,ROUND(fo.commission_rate, 2) as commission_rate
-	  ,ROUND(fo.commission_value,2) as commission_value
-	  ,ROUND(fo.order_value - fo.mexFunded + fo.tax_value - fo.commission_value, 2) as net_payable
+	  ,ROUND(fo.mex_commission,2) as commission_value
+	  ,ROUND(fo.order_value_with_tax - coalesce(mex_funded_fo.total_mex_funded_discount,0) - fo.mex_commission, 2) as net_payable
 	  --,case when fo.country_id = 4 then round(round((coalesce(fo.sub_total,bb.food_sub_total) * (1 + fo.tax)),2) - round((coalesce(fo.sub_total,bb.food_sub_total) * fo.commission * 1.07),2), 2) else round(round((coalesce(fo.sub_total,bb.food_sub_total) * (1 + fo.tax)),2) - round((coalesce(fo.sub_total,bb.food_sub_total) * fo.commission),2), 2) end as net_payable_mex
 
 	  --pay status
@@ -87,6 +126,18 @@ SELECT
       ,case when json_extract_scalar(fo.metadata, '$.qsrCode') = 'mcd' then 'QSR POS Integrated' end as qsr_pos_flag
       ,json_extract_scalar(fo.metadata, '$.qsrCode') as qsr_pos_integration_type
       ,case when json_extract_scalar(fo.metadata, '$.partnerOrderSubmitTime') is not null then 1 else 0 end as qsr_pos_is_submitted
+      ,case when json_extract_scalar(fo.metadata, '$.payMerchant') is not null then 1 else 0 end as qsr_pay_merchant
+
+       ,case when fo.metadata <> '' then
+        case when json_extract_scalar(json_parse(fo.metadata), '$.FPTAcceptedBy') = '3' or aa.auto_accept_order is not null then 'mex-auto-accept' end
+        else null end as my_mex_auto_accept
+    ,case when fo.metadata <> '' then
+        case
+            when json_extract_scalar(json_parse(fo.metadata), '$.FPTAcceptedBy') = '3' or aa.auto_accept_order is not null then 'mex-accept'
+            when coalesce(fo.pre_accepted_time,fo.ord_order_in_prepare_time) IS NOT NULL then 'mex-accept'
+            end
+        when coalesce(fo.pre_accepted_time,fo.ord_order_in_prepare_time) IS NOT NULL then 'mex-accept'
+        else NULL end as my_mex_accept_column
 
 
 
@@ -96,24 +147,11 @@ SELECT
       {{#endif}}
 
     --Food order table
-    FROM (select *
-            ,cast(json_extract(json_parse(snapshot_detail), '$.cartWithQuote.foodQuoteInMin.priceInMinorUnit') as double)/100 as order_value
-	          ,coalesce(cast(json_extract_scalar(cast(json_extract(json_parse(snapshot_detail), '$.cartWithQuote.discounts') as array<json>)[1], '$.mexFundedAmountInMin') as double), 0)/100 as mexFunded
-	          ,coalesce(cast(json_extract(cast(json_extract(json_parse(snapshot_detail), '$.cartWithQuote.merchantCartWithQuoteList') as array<json>)[1], '$.merchantInfoObj.taxRate') as double), prejoin_food_order.tax) as tax_perc
-	          ,coalesce(cast(json_extract_scalar(snapshot_detail, '$.cartWithQuote.foodQuoteInMin.inclTaxInMin') as double),0)/100  as tax_value
-	          ,coalesce(cast(json_extract(cast(json_extract(json_parse(snapshot_detail), '$.cartWithQuote.merchantCartWithQuoteList') as array<json>)[1], '$.merchantInfoObj.commission') as double), prejoin_food_order.commission) as commission_rate
-	          ,coalesce(cast(json_extract(snapshot_detail, '$.cartWithQuote.foodQuoteInMin.mexCommission') as double),0)/100 AS commission_value
-            ,coalesce(json_extract_scalar(snapshot_detail, '$.cartWithQuote.promoCodes[0].promoCode'),'') as promo_code
-            ,coalesce(cast(json_extract_scalar(json_parse(snapshot_detail), '$.cartWithQuote.promoCodes[0].promoAmountInMin') as double),0) / power(double '10.0', coalesce(cast(json_extract_scalar(snapshot_detail, '$.currency.exponent') as int),0))
-			    as promo_expense
-            from public.prejoin_food_order
-            where  [[country_id in ({{country|noquote}})]]
-            and [[city_id in ({{cities|noquote}})]]
-            and [[merchant_id in ({{merchant}})]]
-            and [[partition_date >= date_format(date({{ order_create_start_date }}) - interval '1' DAY, '%Y-%m-%d')]]
-            and [[partition_date <= date_format(date({{ order_create_end_date }}) + interval '1' DAY , '%Y-%m-%d')]]) fo
+    FROM fo
     --base bookings
-    left join datamart.base_bookings bb on fo.order_id = bb.order_id
+    left join datamart_grabfood.base_bookings bb on fo.order_id = bb.order_id
+
+    left join mex_funded_fo on fo.order_id = mex_funded_fo.order_id
 
     -- GP txn details
     left join
@@ -171,23 +209,16 @@ SELECT
     --SG & MY
     --and fo.country_id in (1,3,4)
     and bb.vertical = 'GrabFood'
-    and [[fo.country_id in ({{country|noquote}})]]
-    and [[fo.city_id in ({{cities|noquote}})]]
-    and [[fo.merchant_id in ({{merchant}})]]
+    and [[bb.country_id in ({{country|noquote}})]]
+    and [[bb.city_id in ({{cities|noquote}})]]
+    and [[bb.merchant_id in ({{merchant}})]]
     and [[mex.chain_number in ({{chain_number}})]]
     and [[date(from_utc_timestamp(fo.created_time, cities.time_zone)) >= date({{order_create_start_date}})]]
     and [[date(from_utc_timestamp(fo.created_time, cities.time_zone)) <= date({{order_create_end_date}})]]
-    and [[fo.partition_date >= date_format(date({{ order_create_start_date }}) - interval '1' DAY, '%Y-%m-%d')]]
-    and [[fo.partition_date <= date_format(date({{ order_create_end_date }}) + interval '1' DAY , '%Y-%m-%d')]]
+    -- and [[fo.partition_date >= date_format(date({{ order_create_start_date }}) - interval '1' DAY, '%Y-%m-%d')]]
+    -- and [[fo.partition_date <= date_format(date({{ order_create_end_date }}) + interval '1' DAY , '%Y-%m-%d')]]
     and [[date(from_utc_timestamp(fo.ord_completed_time, cities.time_zone)) >= date(date_format(date({{ order_delivered_start_date }}) - interval '1' DAY, '%Y-%m-%d'))]]
     and [[date(from_utc_timestamp(fo.ord_completed_time, cities.time_zone)) <= date(date_format(date({{ order_delivered_end_date }}) , '%Y-%m-%d'))]]
 
     and [[date(bb.date_local) >= date({{order_create_start_date}})]]
     and [[date(bb.date_local) <= date({{order_create_end_date}})]]
-    {{#if delivery_option == 'takeaway'}}
-		AND json_extract_scalar(fo.snapshot_detail, '$.deliveryOption') = '1'
-		{{#endif}}
-		{{#if delivery_option == 'delivery'}}
-		AND (json_extract_scalar(fo.snapshot_detail, '$.deliveryOption') = '0'
-		OR json_extract_scalar(fo.snapshot_detail, '$.deliveryOption') is null)
-		{{#endif}}
